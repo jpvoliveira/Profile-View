05fac408324059fe7c005f8d3ff6b9b894698143
João Pedro Vitor de Oliveira <joao.pvo44@gmail.com>
2024-06-11 18:53:38 -0300

    feat: conteudo sugerido sendo consumido do json-server

--- assets/js/content.js
+++ assets/js/content.js
@@ -4,13 +4,48 @@ const contentService = new ContentService();
 
 async function setupContentSection() {
     const contents = await contentService.getContents();
+    let contContent = 0;
 
-    contents.forEach(content => {
-        createContentComponent(content)
-    });
+    if (contents.length > 0) {
+        contents.forEach(content => {
+            createContentComponent(content, contContent)
+            contContent++
+        });
+    }
 }
 
-function createContentComponent(content) {
+function createContentComponent(content, contContent) {
+    const carouselItem = document.createElement('div');
+
+    carouselItem.classList.add('carousel-item');
+    carouselItem.style.height = '400px';
+
+    if (contContent === 0) {
+        carouselItem.classList.add('active');
+    }
+
+    const img = document.createElement('img');
+    img.src = content.image_url;
+    img.alt = content.title;
+    img.style.height = '400px';
+    img.style.width = '100%';
+
+    const captionDiv = document.createElement('div');
+    captionDiv.classList.add('carousel-caption', 'd-none', 'd-md-block');
+
+    const link = document.createElement('a');
+    link.href = content.content_url;
+    link.target = '_blank';
+
+    const title = document.createElement('h4');
+    title.textContent = content.title;
+
+    link.appendChild(title);
+    captionDiv.appendChild(link);
+    carouselItem.appendChild(img);
+    carouselItem.appendChild(captionDiv);
+
+    document.querySelector('.carousel-inner').appendChild(carouselItem);
 }
 
 window.addEventListener("load", setupContentSection);
